ff9ecb1bd3b24390f283c7be78f0fc0332fde4bc
haidy61 <ahmedhaidy252@gmail.com>
2022-06-24 23:15:42 +0200

    adding the tool-bar options

--- src/text-editor.js
+++ src/text-editor.js
@@ -9,3 +9,18 @@ import { useParams } from 'react-router-dom';
 import logo from './HomePageLogo.png'
 const HEROKU_ADD = 'https://write-it-editor-server.herokuapp.com/';
 
+//the toolbar options that the user can use
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],   //header values to show different font sizes
+            [{ font: [] }],  //different font types
+            [{ list: "ordered" }, { list: "bullet" }],  //ordered&unordered list
+            [{ 'direction': 'rtl' }],   //left or right placement of text
+            [{ 'indent': '-1'}, { 'indent': '+1' }],   //moving the text
+            [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }], //alignement of text
+            [{ size: [ 'small', false, 'large', 'huge' ]}], //font sizes different from the headers
+            ["bold", "italic", "underline"], //for different looks of text
+            [{ color: [] }, { background: [] }], //to change text color or the background color
+            [{ script: "sub" }, { script: "super" }], //for making superscript or subscript
+            ["image", "blockquote", "code-block","link","video","formula"], //to add various options rather than text
+            ["clean"],
+];
