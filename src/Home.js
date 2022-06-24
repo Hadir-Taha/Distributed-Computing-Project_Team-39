ab480be5f4a92037a6beef40cad049a3aa6aaa70
AliaaKaram <aliaakaram337@yahoo.com>
2022-06-24 22:49:52 +0200

    "Add Imports of Home Page file"

---
+++ src/Home.js
@@ -0,0 +1,9 @@
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+import Modal from 'react-bootstrap/Modal'
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import './Home.css';
+import logo from './HomePageLogo.png'
