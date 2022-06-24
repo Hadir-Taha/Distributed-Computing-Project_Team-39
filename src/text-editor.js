ebad7b07215de0a39a66a69ea60f582839b33772
haidy61 <ahmedhaidy252@gmail.com>
2022-06-24 23:14:07 +0200

    Adding the imports of the text editor

--- src/text-editor.js
+++ src/text-editor.js
@@ -1,1 +1,10 @@
-import from
+import { useCallback, useEffect, useState } from 'react';
+import 'quill/dist/quill.snow.css';
+import io from 'socket.io-client';
+import Quill from 'quill';
+import Button from 'react-bootstrap/Button';
+import Toast from 'react-bootstrap/Toast'
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import logo from './HomePageLogo.png'
+
