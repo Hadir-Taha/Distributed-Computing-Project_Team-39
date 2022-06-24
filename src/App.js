42f656a8ec0c0a57b2c16857a0d190265f1cf0b6
AliaaKaram <aliaakaram337@yahoo.com>
2022-06-24 22:12:15 +0200

    "Add imports in the App.js file"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import TextEditor from './text-editor';
+import HomeScreen from './Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
   );
 }
 
