44217ec706821fe7ad69abf74e92733d3b6a0903
haidy61 <ahmedhaidy252@gmail.com>
2022-06-25 15:59:41 +0200

    adding comments in the updated version of text editor

--- src/text-editor.js
+++ src/text-editor.js
@@ -11,36 +11,35 @@ import logo from './HomePageLogo.png'
 const HEROKU_ADD = 'https://fresh-edit-server.herokuapp.com/';
 
 const TOOLBAR_OPTIONS = [
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],   //header values to show different font sizes
+  [{ header: [1, 2, 3, 4, 5, 6, false] }], //different heading styles
             [{ font: [] }],  //different font types
-            [{ list: "ordered" }, { list: "bullet" }],  //ordered&unordered list
-            [{ 'direction': 'rtl' }],   //left or right placement of text
-            [{ 'indent': '-1'}, { 'indent': '+1' }],   //moving the text
-            [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }], //alignement of text
-            [{ size: [ 'small', false, 'large', 'huge' ]}], //font sizes different from the headers
-            ["bold", "italic", "underline"], //for different looks of text
+            [{ list: "ordered" }, { list: "bullet" }],  //lists types
+            [{ 'direction': 'rtl' }],   //left or right placement of text to be able to write different lang.
+            [{ 'indent': '-1'}, { 'indent': '+1' }],   //indentation of text
+            [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }], //alignment of text
+            [{ size: [ 'small', false, 'large', 'huge' ]}], //another font sizes
+            ["bold", "italic", "underline"], //for different styling of font
             [{ color: [] }, { background: [] }], //to change text color or the background color
             [{ script: "sub" }, { script: "super" }], //for making superscript or subscript
             ["image", "blockquote", "code-block","link","video","formula"], //to add various options rather than text
             ["clean"],
 ];
-
 const TextEditor = () => {
-  const [socket, setSocket] = useState();
-  const [quill, setQuill] = useState();
-  const { id: documentId } = useParams();
-  const [show, setShow] = useState(false);
-  // setting up the editor
+  const [socket, setSocket] = useState(); //variable socket creation with empty initial value
+  const [quill, setQuill] = useState();  //variable quill creation with empty initial value
+  const { id: documentId } = useParams(); //variable id creation that will hold the id of each document
+
+  // creation of the layout of the editor using react-hook functions
   const WrapperRef = useCallback((wrapper) => {
     if (wrapper === null) return;
     wrapper.innerHTML = '';
     const editor = document.createElement('div');
     wrapper.append(editor);
-    const q = new Quill(editor, {
-      theme: 'snow',
+    const q = new Quill(editor, {   //creation of q variable that hold tha quill with the following features
+      theme: 'snow',   //to add the tool bar
       modules: { toolbar: TOOLBAR_OPTIONS },
       history: {
-        delay: 2000,
+        delay: 1500,
         userOnly: true,
       },
     });
@@ -49,34 +48,34 @@ const TextEditor = () => {
     setQuill(q);
   }, []);
 
-  // Setting up the connection to server
+  //connection of the server using socket.io functionalities & react-hook
   useEffect(() => {
-    const s = io(HEROKU_ADD);
+    const s = io(HEROKU_ADD);  //creating a variable that will hold the URL of the server
     setSocket(s);
     return () => {
       s.disconnect();
     };
   }, []);
 
-  // capturing the changes and sending it to the server
+  // Recieving the updates and sending them to the server
   useEffect(() => {
-    if (socket == null || quill == null) return;
-    const handler = (delta, oldDelta, source) => {
+    if (socket == null || quill == null) return; //check whether socket & quill have values or no
+    const handler = (delta, oldDelta, source) => {  //handler fn creation with the parameters indicating the updated data, old data & user
       if (source !== 'user') return;
-      socket.emit('send-changes', delta);
+      socket.emit('Transfer-updates to server', delta); //socket send delta(updates) to the server
     };
-    quill.on('text-change', handler);
+    quill.on('New-text added', handler); //wait until new updates are made and transfer if to handler
 
     return () => {
-      quill.off('text-change', handler);
+      quill.off('New-text added', handler);
     };
   }, [socket, quill]);
 
-  // update content with the changes made
+  // update the tenor of the document
   useEffect(() => {
-    if (socket == null || quill == null) return;
+    if (socket == null || quill == null) return; //check whether socket & quill have values or no
     const handler = (delta) => {
-      quill.updateContents(delta);
+      quill.updateContents(delta);  //transfer updates to all users
     };
     socket.on('receive-changes', handler);
 
@@ -85,10 +84,10 @@ const TextEditor = () => {
     };
   }, [socket, quill]);
 
-  useEffect(() => {
+  useEffect(() => {  //for saving new content of document every 1.5 seconds
     const interval = setInterval(() => {
-      socket.emit('save-doc', quill.getContents());
-    }, 2000);
+      socket.emit('saving-document', quill.getContents());
+    }, 1500);
 
     return () => {
       clearInterval(interval);
@@ -106,16 +105,19 @@ const TextEditor = () => {
     socket.emit('get-document', documentId);
   }, [socket, quill, documentId]);
 
-  // div having the editor
+  // html for the text editor
   return (
 <div className="home-card">
+//button to copy the id of the document
 <Button className="like_button" onClick={() => {
         setShow(true)
         navigator.clipboard.writeText(documentId)
       }} style={{ "position": "absolute", "right": 0 }}>
         Copy ID
       </Button>
+      //button the refer to the main home page
       <Link to="/" className="like_button" style={{ "position": "absolute", "left": 0  }}>Home</Link>
+      //div for holding the main text editor
       <div className="container" ref={WrapperRef}></div>
   </div>
   );
