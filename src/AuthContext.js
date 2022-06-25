901fe12c49fd247be60090b8c7bf574df5dd2c67
haidy61 <ahmedhaidy252@gmail.com>
2022-06-26 00:03:17 +0200

    authentication functions for login & logout

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -13,3 +13,10 @@ export function AuthProvider({ children }) {
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password)
   }
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password)
+  }
+
+  function logout() {
+    return auth.signOut()
+  }
