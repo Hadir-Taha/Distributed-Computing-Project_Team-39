434f5f751b7075b120777132f628b202016e60bf
haidy61 <ahmedhaidy252@gmail.com>
2022-06-26 00:06:22 +0200

    Adding new imports for the new files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import TextEditor from './text-editor';
-import HomeScreen from './Home';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom';
+import React from "react"
+import { Link } from 'react-router'
 import { useState } from 'react';
+import Signup from "./Signup"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "./AuthContext"
+import { BrowserRouter as Router, Switch, Route , Redirect} from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from "./Login"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 import { v4 as uuidV4 } from 'uuid';
+import HomeScreen from "./Home"
+import TextEditor from "./text-editor"
+
 //defining th different paths & where each path will lead us to
 //path "/" will lead us to the home page
 //path "/rooms" will lead us to the texteditor but with random id
