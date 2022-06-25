552df403e66980ed3592380e62bf2c6fbd019045
Aliaa <aliaakaram750@gmail.com>
2022-06-25 05:44:40 +0200

    "Updating some functions in text Editor file"

--- src/text-editor.js
+++ src/text-editor.js
@@ -1,38 +1,34 @@
 import { useCallback, useEffect, useState } from 'react';
 import 'quill/dist/quill.snow.css';
-import io from 'socket.io-client';
 import Quill from 'quill';
+import io from 'socket.io-client';
 import Button from 'react-bootstrap/Button';
 import Toast from 'react-bootstrap/Toast'
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
+
 import logo from './HomePageLogo.png'
-const HEROKU_ADD = 'https://write-it-editor-server.herokuapp.com/';
-const HEROKU_ADD2='https://alternative-server.herokuapp.com/';
-//the toolbar options that the user can use
+const HEROKU_ADD = 'https://fresh-edit-server.herokuapp.com/';
+
 const TOOLBAR_OPTIONS = [
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],   //header values to show different font sizes
-            [{ font: [] }],  //different font types
-            [{ list: "ordered" }, { list: "bullet" }],  //ordered&unordered list
-            [{ 'direction': 'rtl' }],   //left or right placement of text
-            [{ 'indent': '-1'}, { 'indent': '+1' }],   //moving the text
-            [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }], //alignement of text
-            [{ size: [ 'small', false, 'large', 'huge' ]}], //font sizes different from the headers
-            ["bold", "italic", "underline"], //for different looks of text
-            [{ color: [] }, { background: [] }], //to change text color or the background color
-            [{ script: "sub" }, { script: "super" }], //for making superscript or subscript
-            ["image", "blockquote", "code-block","link","video","formula"], //to add various options rather than text
-            ["clean"],
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ size: ['small', false, 'large', 'huge'] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
 ];
-//Creation of the object of the texteditor component
+
 const TextEditor = () => {
-//create socket variable using react-hook functionality
   const [socket, setSocket] = useState();
- //create quill variable using react-hook functionality
   const [quill, setQuill] = useState();
- //return ID from URL & store it in the documentID using Useparams()
   const { id: documentId } = useParams();
-  //set up the editor
+
+  // setting up the editor
   const WrapperRef = useCallback((wrapper) => {
     if (wrapper === null) return;
     wrapper.innerHTML = '';
@@ -50,29 +46,31 @@ const TextEditor = () => {
     q.setText('');
     setQuill(q);
   }, []);
- // prepare the server socket
+
+  // Setting up the connection to server
   useEffect(() => {
-    const s = io(HEROKU_ADD); // s variable that store the server url
-    setSocket(s);             // set the socket to the variable s
+    const s = io(HEROKU_ADD);
+    setSocket(s);
     return () => {
       s.disconnect();
     };
   }, []);
-  // hold the updates made by the user and transfer it to the server
+
+  // capturing the changes and sending it to the server
   useEffect(() => {
-    if (socket == null || quill == null) return;   // check if the socket and the quill have values (not null)
-    const handler = (delta, oldDelta, source) => { // create handler function with parameters (delta->new data,oldDelta->old data writen in the editor,source->user)
+    if (socket == null || quill == null) return;
+    const handler = (delta, oldDelta, source) => {
       if (source !== 'user') return;
-      socket.emit('send-changes', delta);          // send updates to the server
+      socket.emit('send-changes', delta);
     };
-    quill.on('text-change', handler); // listen to the updates made by the user & send it to the handler which will then send it to the server (socket)
+    quill.on('text-change', handler);
+
     return () => {
       quill.off('text-change', handler);
     };
   }, [socket, quill]);
 
-
-// Reflecting changes to the document, the same idea as broadcasting changes to all the users that share that document
+  // update content with the changes made
   useEffect(() => {
     if (socket == null || quill == null) return;
     const handler = (delta) => {
@@ -85,39 +83,39 @@ const TextEditor = () => {
     };
   }, [socket, quill]);
 
-//Save the contents of document every 1.5 seconds
   useEffect(() => {
     const interval = setInterval(() => {
       socket.emit('save-doc', quill.getContents());
-    }, 1500);
+    }, 2000);
+
     return () => {
       clearInterval(interval);
     };
   }, [socket, quill]);
 
-useEffect(() => {
+  useEffect(() => {
     if (socket == null || quill == null) return;
+
     socket.once('load-document', (document) => {
       quill.setContents(document);
       quill.enable();
     });
+
     socket.emit('get-document', documentId);
   }, [socket, quill, documentId]);
-  //adding the last stepup of the editor
-return (
-<div>
-//creating button that will copy the ID of the document
-<Button variant="danger" onClick={() => {
-navigator.clipboard.writeText(documentId)
-}} style={{ "position": "fixed", "right": 0, "margin-right": "20px" ,"background-color":" #e75480"}}>
-Copy ID
-</Button>
-//creating a button that will refer to the home page back again
-<Link to="/" className="btn btn-danger" style={{ "position": "fixed", "margin-left": "30px","background-color":" #e75480" }}>Home</Link>
-//add a div for placing the main editor in it with the toolbar & everything related to it
-<div className="container" ref={WrapperRef}></div>
-</div>
-);
+
+  // div having the editor
+  return (
+    <div>
+      <Button variant="danger" onClick={() => {
+        navigator.clipboard.writeText(documentId)
+      }} style={{ "position": "absolute", "right": 0, "margin-right": "20px" }}>
+        Copy Document ID
+      </Button>
+      <Link to="/" className="btn btn-danger" style={{ "position": "absolute", "margin-left": "110px" }}>Home</Link>
+      <div className="container" ref={WrapperRef}></div>
+    </div>
+  );
 };
 
 export default TextEditor;
