a5baf9c903956d6d4181d71683f4ac855b343242
Aliaa <aliaakaram750@gmail.com>
2022-06-25 04:22:59 +0200

    "Updating"

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import TextEditor from './text-editor';
+import HomeScreen from './Home';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+
+function App() {
+    const [docId, setDocId] = useState('');
+  return (
+      <Router>
+      <Routes>
+               <Route path="/" element={ <HomeScreen docId={docId} setDocId={setDocId} />} />
+               <Route path="/rooms" element={  <Navigate to={`/rooms/documents/${uuidV4()}`} />} />
+               <Route path="/rooms/documents/:id" element={ <TextEditor />} />
+      </Routes>
+    </Router>
+
+  );
+}
+
+export default App;
